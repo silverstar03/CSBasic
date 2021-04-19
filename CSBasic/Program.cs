281b2d6b346c63f2003c3ffd0b0f225b06d7a46e
silverstar03 <eunbyoul0421@naver.com>
2021-04-19 15:41:15 +0900

    #6 복합 대입 연산자 문자열 관련 복합 대입 연산자

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -113,6 +113,27 @@ namespace CSBasic
 
             bool TrueOrFalse = true;
 
+
+            int output = 0;
+            output += 52;   //output = output + 52;
+            output += 273;  
+            output += 103;
+
+            Console.WriteLine(output);
+
+            output *= 2;
+            Console.WriteLine(output);
+
+
+            //문자열 관련 복합 대입 연산자
+            string output2 = "Hello";
+            output2 += "World";
+            output2 += "!";
+            Console.WriteLine(output2);
+
+
+
+
         }
     }
 }
