eaadbc8e9171229b95eba43f78c82c1e9aaab8ff
silverstar03 <eunbyoul0421@naver.com>
2021-04-21 10:01:00 +0900

    #11 Type Casting Practice

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -177,7 +177,7 @@ namespace CSBasic
             while (true)
             {
                 input = Console.ReadLine();
-                if(input != "q")
+                if (input != "q")
                 {
                     Console.WriteLine(input);
                 }
@@ -186,6 +186,97 @@ namespace CSBasic
                     break;
                 }
             }
+
+            //강제 형변환
+            //강제 형변환은 항상 데이터 손실이 일어나는 것은 아니다.
+            /*long longNumber = 2147483647L + 2147483647L;*/
+            long longNumber = 100;
+            int intNumber = (int) longNumber;
+            Console.WriteLine(intNumber);
+
+
+            //자동 형변환
+            intNumber = 100;
+            longNumber = intNumber;
+            Console.WriteLine(longNumber);
+
+            // int -> long, float, double;
+            // long -> float, double;
+            // char -> int, long, float, double
+            // float -> double
+
+            //Widening Casting 자동 형변환
+            //순서 외워두기
+            //byte -> short -> char -> int -> long -> float -> double
+
+            //Narrowing Casting 강제 형변환
+            //double -> float -> long -> int -> char -> short -> byte
+
+            //int.Parse() / long.Parse()
+            //float.Parse() / double.Parse()
+            string numberString = "52273";
+            //intNumber = (int)numberString;
+            intNumber = int.Parse(numberString);
+            Console.WriteLine(intNumber);
+
+            try
+            {
+
+                Console.WriteLine(int.Parse("52.273"));
+                Console.WriteLine(int.Parse("오십일"));
+                Console.WriteLine(int.Parse("Seven"));
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("숫자로 변경할 수 없습니다.");
+            }catch(Exception e)
+            {
+                Console.WriteLine("알 수 없는 오류: " + e.Message);
+            }
+            //다른 자료형을 문자열로 변환
+            Console.WriteLine((10).ToString());
+            Console.WriteLine((52.237).ToString());
+            Console.WriteLine(('a').ToString());
+            Console.WriteLine((true).ToString());
+            Console.WriteLine((false).ToString());
+
+            double number2 = 52.273103;
+            Console.WriteLine(number2.ToString("0.0"));
+            Console.WriteLine(number2.ToString("0.00"));
+            Console.WriteLine(number2.ToString("0.000"));
+            Console.WriteLine(number2.ToString("0.0000"));
+
+            Console.WriteLine((52 + ""));
+            Console.WriteLine((52 + 3 + ""));
+            Console.WriteLine("" + 52 + 3);
+            Console.WriteLine(52 + "" + 3);
+
+            number = 273;
+            string outputA = number + "";
+            Console.WriteLine(number);
+
+            char ch = 'a';
+            string outputB = ch + "";
+            //string outputB = ch;
+            //string outputB = 3;
+            Console.WriteLine(outputB);
+
+            //string to bool 변환
+            Console.WriteLine(bool.Parse("True"));
+            Console.WriteLine(bool.Parse("true"));
+            Console.WriteLine(bool.Parse("TrUe"));
+            Console.WriteLine(bool.Parse("False"));
+            Console.WriteLine(bool.Parse("false"));
+
+            //최댓값, 최솟값은 상수로
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            Console.WriteLine(-1 * int.MaxValue);
+            //최솟값에 그냥 -1을 곱하면 범위가 벗어남
+            //Console.WriteLine(-1 * int.MinValue);
+            Console.WriteLine(-1L * int.MinValue);
+
 
         }
     }
