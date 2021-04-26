34fb56bf173729fd983f2252ff67d67271ce877e
silverstar03 <eunbyoul0421@naver.com>
2021-04-26 15:49:56 +0900

    #14 삼항연산자 예제

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -90,6 +90,11 @@ namespace CSBasic2
                     Console.WriteLine("지구인가요?");
                     break;
             }
+
+            Console.WriteLine("자연수 판별기");
+            Console.Write("숫자를 입력하세요:");
+            int number7 = int.Parse(Console.ReadLine());
+            Console.WriteLine((number7 > 0)?"자연수":"자연수 아님");
         }
     }
 }
