25ca0b9d75ef67a74cfa8c016c267bbece08ee22
silverstar03 <eunbyoul0421@naver.com>
2021-08-23 14:50:49 +0900

    초기 커밋.

---
+++ Chapter6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter6
+{
+    class Animal
+    {
+        public int Age { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
