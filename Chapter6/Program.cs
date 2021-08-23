842e38a7ab023a0d95e4154ee66286e61064e530
silverstar03 <eunbyoul0421@naver.com>
2021-08-23 16:09:46 +0900

    쉐도잉, 하이딩, 오버라이딩, 추상 클래스, 추상 메서드 예제

--- Chapter6/Program.cs
+++ Chapter6/Program.cs
@@ -27,8 +27,52 @@ namespace Chapter6
     }
     class Program
     {
+        abstract class AbstractParent
+        {
+            public abstract void Test();
+        }
+        class AbstractChild : AbstractParent
+        {
+            public override void Test() { }
+        }
+        class Parent
+        {
+            public int variable = 273;
+            public void Method()
+            {
+                Console.WriteLine("부모의 메서드");
+            }
+        }
+        class Child : Parent
+        {
+            public new String variable = "shadowing";
+            public new void Method()
+            {
+                Console.WriteLine("자식의 메서드");
+            }
+        }
         static void Main(string[] args)
         {
+            Child child = new Child();
+            child.Method();
+            ((Parent)child).Method();
+
+            //Console.WriteLine((new Child()).variable);
+            //Console.WriteLine(((Parent)(new Child())).variable);
+
+            /*
+            List<Animal> Animals = new List<Animal>()
+            {
+                new Dog(), new Cat(), new Cat(), new Dog(),
+                new Dog(), new Cat(), new Cat(), new Dog()
+            };
+            foreach(var item in Animals)
+            {
+                item.Eat();
+                item.Sleep();
+                ((Cat)item).Meow();
+            }
+            */
         }
     }
 }
