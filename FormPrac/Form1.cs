8477256ba248444f1bd4da444b2c97212df2ffa9
silverstar03 <eunbyoul0421@naver.com>
2021-08-25 10:03:34 +0900

    MessageBox 활용 실습

--- FormPrac/Form1.cs
+++ FormPrac/Form1.cs
@@ -16,5 +16,36 @@ namespace FormPrac
         {
             InitializeComponent();
         }
+
+        private void btnMessageBox1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            DialogResult result = DialogResult.Cancel;
+            if (btn == btnMessageBox1)
+            {
+                result = MessageBox.Show("안녕하세요~");
+            }
+            else if (btn == btnMessageBox2)
+            {
+                result = MessageBox.Show("안녕하세요~", "격하게 환영인사");
+            }
+            else if(btn == btnMessageBox3)
+            {
+                result = MessageBox.Show("안녕하세요~", "격하게 환영인사",
+                    MessageBoxButtons.OKCancel,
+                    MessageBoxIcon.Information);
+            }
+
+            switch(result)
+            {
+                case DialogResult.OK:
+                    MessageBox.Show("나도 반가워요~ ");
+                    break;
+                case DialogResult.Cancel:
+                    MessageBox.Show("나도 안반가워요~ ");
+                    break;
+            }
+            
+        }
     }
 }
