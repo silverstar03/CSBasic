0e1183252063d1cb9be6ecd6058fcb186e74fc02
silverstar03 <eunbyoul0421@naver.com>
2021-09-01 10:14:29 +0900

    윈폼 label, linkedlabel, checkbox 예제

--- CSBasic8-label/Form1.cs
+++ CSBasic8-label/Form1.cs
@@ -15,6 +15,59 @@ namespace CSBasic8_label
         public Form1()
         {
             InitializeComponent();
+
+            Label label = new Label()
+            {
+                Text = "글자",
+                Location = new Point(10, 10)
+            };
+
+            Controls.Add(label);
+
+            LinkLabel linkLabel = new LinkLabel()
+            {
+                Text = "글자",
+                Location = new Point(10, 50)
+            };
+            Controls.Add(linkLabel);
+            linkLabel.Click += LabelClick;
+
+
+            CheckBox checkBox1 = new CheckBox() { Text = "감자", Location = new Point(130, 10) };
+            CheckBox checkBox2 = new CheckBox() { Text = "고구마", Location = new Point(130, 40) };
+            CheckBox checkBox3 = new CheckBox() { Text = "토마토", Location = new Point(130, 70) };
+            Button button = new Button();
+            button.Location = new Point(130, 100);
+            button.Text= "클릭";
+            button.Click += ButtonClick;
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox3);
+            Controls.Add(button);
+
         }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+            foreach(var item in Controls)
+            {
+                if(item is CheckBox)
+                {
+                    CheckBox checkBox = (CheckBox)item;
+                    if(checkBox.Checked)
+                    {
+                        list.Add(checkBox.Text);
+                    }
+                }
+            }
+            MessageBox.Show(string.Join(",", list));
+        }
+        private void LabelClick(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.naver.com");
+            System.Diagnostics.Process.Start("notepad.exe");
+        }
+
     }
 }
---
+++ CSBasic8-label/button/ext.cs
@@ -0,0 +1,6 @@
+﻿namespace button
+{
+    internal class ext
+    {
+    }
+}
