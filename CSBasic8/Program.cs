5c014c193637b7616feace10e8020722ed4eef44
silverstar03 <eunbyoul0421@naver.com>
2021-09-01 09:13:20 +0900

    IComparable 인터페이스 실습

--- CSBasic8/Program.cs
+++ CSBasic8/Program.cs
@@ -8,8 +8,39 @@ namespace CSBasic8
 {
     class Program
     {
-        static void Main(string[] args)
+
+        class Product : IComparable
         {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+            public int CompareTo(object obj)
+            {
+                //return Price.CompareTo((obj as Product).Price);
+                //return -1 * Price.CompareTo((obj as Product).Price); //역정렬
+                return Name.CompareTo((obj as Product).Name);
+
+            }
+
+            public override string ToString()
+            {
+                return Name + ":" + Price + "원";
+            }
+        }
+            static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>()
+            {
+                new Product () {Name="고구마", Price=1500},
+                new Product () {Name="사과", Price=2400},
+                new Product () {Name="바나나", Price=1000},
+                new Product () {Name="배", Price=3000},
+            };
+            list.Sort();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
