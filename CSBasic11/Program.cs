7d9d53f87006ad04007051339addc835fef3c114
silverstar03 <eunbyoul0421@naver.com>
2021-09-15 09:03:56 +0900

    Linq를 활용한 데이터 필터링 및 정렬 예제

--- CSBasic11/Program.cs
+++ CSBasic11/Program.cs
@@ -13,13 +13,18 @@ namespace CSBasic11
             List<int> input = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             List<int> output = new List<int>();
 
-            foreach(var item in input)
+            output = (from item in input
+                      where item % 2 == 0
+                      orderby item descending
+                      select item).ToList();
+
+           /* foreach(var item in input)
             {
                 if(item % 2 == 0)
                 {
                     output.Add(item);
                 }
-            }
+            }*/
             foreach(var item in output)
             {
                 Console.WriteLine(item);
